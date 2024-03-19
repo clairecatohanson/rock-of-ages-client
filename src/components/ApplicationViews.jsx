23de3b555a00e6f6c6a190d0e7941327181fe0cf
Claire Hanson <clairecatohanson@gmail.com>
2024-03-19 15:38:48 -0500

    implemented my rocks and delete rock

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,44 +1,74 @@
-import { useState } from 'react'
+import { useState } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Authorized"
 import { Login } from "../pages/Login.jsx"
 import Home from "../pages/Home"
 import { RockForm } from "./RockForm.jsx"
 import { RockList } from "./RockList.jsx"
-import { Register } from '../pages/Register.jsx'
-
+import { Register } from "../pages/Register.jsx"
 
 export const ApplicationViews = () => {
-    const [rocksState, setRocksState] = useState([{
-        id: 1,
-        name: "Sample",
-        type: {
-            id: 1,
-            label: "Volcanic"
-        }
-    }])
+  const [rocksState, setRocksState] = useState([
+    {
+      id: 0,
+      name: "",
+      type: {
+        label: "Volcanic",
+      },
+      user: {
+        first_name: "",
+        last_name: "",
+      },
+    },
+  ])
 
-    const fetchRocksFromAPI = async () => {
-        const response = await fetch("http://localhost:8000/rocks",
-            {
-                headers: {
-                    Authorization: `Token ${JSON.parse(localStorage.getItem("rock_token")).token}`
-                }
-            })
-        const rocks = await response.json()
-        setRocksState(rocks)
+  const fetchRocksFromAPI = async (rock_list_param) => {
+    const getOptions = {
+      headers: {
+        Authorization: `Token ${
+          JSON.parse(localStorage.getItem("rock_token")).token
+        }`,
+      },
     }
 
-    return <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<Authorized />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/allrocks" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/create" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
-            </Route>
-        </Routes>
+    let response
+    if (rock_list_param == "mine") {
+      response = await fetch(
+        "http://localhost:8000/rocks?owner=current",
+        getOptions
+      )
+    } else {
+      response = await fetch("http://localhost:8000/rocks", getOptions)
+    }
+    const rocks = await response.json()
+    setRocksState(rocks)
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<Authorized />}>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/:rock_list_param"
+            element={
+              <RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />
+            }
+          />
+          <Route
+            path="/create"
+            element={<RockForm fetchRocks={fetchRocksFromAPI} />}
+          />
+          {/* <Route
+            path="/mine"
+            element={
+              <RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />
+            }
+          /> */}
+        </Route>
+      </Routes>
     </BrowserRouter>
+  )
 }
--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,23 +1,73 @@
 import { useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 export const RockList = ({ rocks, fetchRocks }) => {
-    useEffect(() => {
-    }, [])
+  const { rock_list_param } = useParams()
 
-    const displayRocks = () => {
-        if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
-        }
+  useEffect(() => {
+    if (rock_list_param) {
+      fetchRocks(rock_list_param)
+    }
+  }, [rock_list_param])
 
-        return <h3>Loading Rocks...</h3>
+  const handleDelete = async (rockId) => {
+    if (rock_list_param) {
+      const deleteOptions = {
+        method: "DELETE",
+        headers: {
+          Authorization: `Token ${
+            JSON.parse(localStorage.getItem("rock_token")).token
+          }`,
+        },
+      }
+
+      const response = await fetch(
+        `http://localhost:8000/rocks/${rockId}`,
+        deleteOptions
+      )
+
+      if (response.status === 204) {
+        await fetchRocks(rock_list_param)
+      }
+    }
+  }
+
+  const displayRocks = () => {
+    if (rocks && rocks.length && rock_list_param) {
+      return rocks.map((rock) => (
+        <div
+          key={`key-${rock.id}`}
+          className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+        >
+          <div>
+            {rock.name} ({rock.type.label}) weighs {rock.weight} kg
+          </div>
+          <div>
+            In the collection of {rock.user.first_name} {rock.user.last_name}
+          </div>
+          {rock_list_param === "mine" ? (
+            <button
+              className="border p-2 mt-4 bg-slate-500 text-white"
+              onClick={() => {
+                handleDelete(rock.id)
+              }}
+            >
+              Delete
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      ))
     }
 
-    return (
-        <>
-            <h1 className="text-3xl">Rock List</h1>
-            {displayRocks()}
-        </>
-    )
+    return <h3>Loading Rocks...</h3>
+  }
+
+  return (
+    <>
+      <h1 className="text-3xl">Rock List</h1>
+      {displayRocks()}
+    </>
+  )
 }
